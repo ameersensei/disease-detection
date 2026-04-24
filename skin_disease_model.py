a5b6da86bf61809e78fd028b585e4d1ada858560
Pathan Ameer khan <kkhanlb@gmail.com>
2026-04-24 22:58:42 +0530

    complete network first edition

--- skin_disease_model.py
+++ skin_disease_model.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 from  tensorflow.keras.models import Sequential
 from tensorflow.keras import layers
 import os
+from tensorflow.keras.callbacks import EarlyStopping
 
 path = r'P:\skind\Skin_cancer_ISIC_The_International_Skin_Imaging_Collaboration'
 train_path = os.path.join(path, 'train')
@@ -21,13 +22,27 @@ datagen = keras.preprocessing.image.ImageDataGenerator(
     validation_split=0.2
 )
 
-train_generator = datagen.flow_from_directory(
+train_data = datagen.flow_from_directory(
     train_path,
     target_size=(224,224),
     batch_size=32,
     class_mode='categorical',
-    subset='training'
+    subset='training',
+    seed = 42,
+    shuffle = True,
 )
+
+val_data = datagen.flow_from_directory(
+    train_path,
+    target_size = (224,224),
+    batch_size = 32,
+    class_mode = 'categorical',
+    subset = 'validation',
+    seed = 42,
+    shuffle = True,
+)
+
+
 
 model = Sequential([
     layers.Conv2D(32, (3,3), padding = 'same', activation='relu', input_shape=(224,224,3)),
@@ -41,5 +56,20 @@ model = Sequential([
 ])
 
 print(model.summary())
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    train_data,
+    validation_split = val_data,
+    epochs = 20,
+    verbose = 1,
+    callbacks = [EarlyStopping(monitor='val_loss', patience=3)]
+)
+
 
 
